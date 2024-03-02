55b2fae319951e9658c2a17748af9113bf484959
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-03-02 12:10:52 +0500

    Initial Setup

---
+++ ContactUs/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    
+        <nav>
+            <div className='logo'>
+                <img src='/Images/logo.png' alt='logo' />
+            </div>  
+            <ul>
+                <li href="#" >Home</li>
+                <li href="#" >LogIn</li>
+                <li href="#" >Product</li>
+            </ul>
+        </nav>
+  
+  )
+}
+export default Navbar;
