64df47c6c10a2c226d3ddbeea76d5e5728644d99
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-03-02 18:14:41 +0500

    Building the Header

--- ContactUs/src/components/ContactHeader/ContactHeader.jsx
+++ ContactUs/src/components/ContactHeader/ContactHeader.jsx
@@ -1,36 +1,38 @@
 import React from 'react'
 import './ContactHeader.css'
+import { IoCall } from "react-icons/io5";
+import { MdOutlineChat } from "react-icons/md";
 
 const ContactHeader = () => {
   return (
     <div>
-     <div>
+     <div className='heading'>
         <h1>CONTACT US</h1>
         <p>LET’S CONNECT: WE’RE HERE TO HELP, AND WE’D LOVE TO HEAR FROM YOU! WHETHER YOU HAVE A QUESTION, COMMENT, OR JUST WANT TO CHAT , YOU CAN REACH OUT TO US THROUGH THE CONTACT FORM OF THIS PAGE, OR BY PHONE, EMAIL, OR SOCIAL MEDIA. </p>
      </div >
         <div className='split' >
             <div>
                 <div>
-                    <div>
-                        <button>VIA SUPPORT CHAT</button>
-                        <button>VIA CALL</button>
+                    <div className='btn-g'>
+                        <button className='btn'><MdOutlineChat /> VIA SUPPORT CHAT</button>
+                        <button className='btn'><IoCall /> VIA CALL</button>
                     </div>
-                    <button>VIA EMAIL FORM</button>
+                    <button className='btn-lg'><MdOutlineChat /> VIA EMAIL FORM</button>
                 </div>
                 <form>
                     <div className='uform'>
                         <label>Name</label>
-                        <input />
+                        <input className='input' />
                     </div>
                     <div className='uform'>
-                        <label>E-Mail</label>
-                        <input />
+                        <label >E-Mail</label>
+                        <input className='input' />
                     </div>
                     <div className='uform'>
                         <label>Text</label>
-                        <input />
+                        <input className='input-text' />
                     </div>
-                    <button>Submit</button>
+                    <button className='btn btn-sub'>Submit</button>
                 </form>
             </div>
             <div>
